5063b47360b4d1cbf7fc124133d868a833d314fc
juwowa09 <juwowa09@gmail.com>
2024-08-05 18:29:48 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const UI());
+}
+
+class UI extends StatelessWidget {
+  const UI({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
