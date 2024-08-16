3ed69a4efb2df379f01c42d6a2ca2d30e9f27314
YuJin <tokyj515@gmail.com>
2024-08-16 16:39:13 +0900

    fix: conflict 해결2

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,12 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import flutter_blue_plus
-import path_provider_foundation
 import geolocator_apple
-
+import path_provider_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FlutterBluePlusPlugin.register(with: registry.registrar(forPlugin: "FlutterBluePlusPlugin"))
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
 }
