ffd5c4dfa5dda398007e36bf78205fabbce50770
juwowa09 <juwowa09@gmail.com>
2024-08-19 17:32:46 +0900

    ju: 안내 가로 스크롤

--- lib/home.dart
+++ lib/home.dart
@@ -476,75 +476,95 @@ class HorizontalScrollableCards extends StatelessWidget {
       appBar: AppBar(
         title: Text('Horizontal Scrollable Cards'),
       ),
-      body: Column(
+      body: Stack(
         children: [
-          Expanded(
-            child: Container(
+          Container(
               color: Colors.grey[300],
               child: Center(child: Text('지도 영역')),
-            ),
           ),
-          Container(
-            padding: EdgeInsets.all(10.0),
-            height: 200.0,
-            child: ListView(
-              scrollDirection: Axis.horizontal,
-              children: <Widget>[
-                buildCard('티맵추천', '37분', '오후 4:26 도착', '13km', '통행료 없음'),
-                buildCard('최소시간', '37분', '오후 4:26 도착', '13km', '통행료 없음'),
-                buildCard('무료도로', '44분', '오후 4:33 도착', '13km', '통행료 없음'),
-              ],
-            ),
-          ),
+
+              Align(
+                alignment: Alignment(0.5,0.8),
+                child: Container(
+                  color:Colors.transparent,
+                  padding: EdgeInsets.all(10.0),
+                  height: MediaQuery.of(context).size.height * 0.2, // 20%,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: <Widget>[
+                      buildCard('티맵추천', '37분', '오후 4:26 도착', '13km', '통행료 없음',context),
+                      buildCard('최소시간', '37분', '오후 4:26 도착', '13km', '통행료 없음',context),
+                      buildCard('무료도로', '44분', '오후 4:33 도착', '13km', '통행료 없음',context),
+                    ],
+                  ),
+                ),
+              ),
+              Align(
+                alignment: Alignment(0, 1),
+                child: GestureDetector(
+                  onTap:(){print('hi');},
+                  child: Container(
+                    margin:EdgeInsets.all(15),
+                    width:double.infinity,
+                    height:MediaQuery.of(context).size.height * 0.05,
+                    decoration: BoxDecoration(color:Color(0xFF2B6AFF),borderRadius: BorderRadius.circular(10)),
+                    child: Center(child: Text(style: TextStyle(color:Colors.white),'안내시작')),
+                  ),
+                ),
+              ),
+
         ],
       ),
     );
   }
 
-  Widget buildCard(String title, String time, String arrival, String distance, String toll) {
-    return Container(
-      width: 150.0,
-      margin: EdgeInsets.symmetric(horizontal: 8.0),
-      padding: EdgeInsets.all(8.0),
-      decoration: BoxDecoration(
-        color: Colors.white,
-        borderRadius: BorderRadius.circular(10.0),
-        boxShadow: [
-          BoxShadow(
-            color: Colors.grey.withOpacity(0.5),
-            spreadRadius: 2,
-            blurRadius: 5,
-            offset: Offset(0, 3),
-          ),
-        ],
-      ),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: <Widget>[
-          Text(
-            title,
-            style: TextStyle(
-                color: Colors.blue, fontSize: 16, fontWeight: FontWeight.bold),
-          ),
-          SizedBox(height: 8.0),
-          Text(
-            time,
-            style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
-          ),
-          Text(
-            arrival,
-            style: TextStyle(fontSize: 14, color: Colors.grey),
-          ),
-          SizedBox(height: 8.0),
-          Text(
-            distance,
-            style: TextStyle(fontSize: 14),
-          ),
-          Text(
-            toll,
-            style: TextStyle(fontSize: 14, color: Colors.grey),
-          ),
-        ],
+  Widget buildCard(String title, String time, String arrival, String distance, String toll, context) {
+    return GestureDetector(
+      onTap: (){
+        print('hi');
+      },
+      child: Container(
+        width: MediaQuery.of(context).size.width * 0.3,
+        margin: EdgeInsets.symmetric(horizontal: 8.0),
+        padding: EdgeInsets.all(8.0),
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(10.0),
+          boxShadow: [
+            BoxShadow(
+              color: Colors.grey.withOpacity(0.5),
+              spreadRadius: 2,
+              blurRadius: 5,
+              offset: Offset(0, 3),
+            ),
+          ],
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text(
+              title,
+              style: TextStyle(
+                  color: Colors.blue, fontSize: 16, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              time,
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+            ),
+            Text(
+              arrival,
+              style: TextStyle(fontSize: 14, color: Colors.grey),
+            ),
+            Text(
+              distance,
+              style: TextStyle(fontSize: 14),
+            ),
+            Text(
+              toll,
+              style: TextStyle(fontSize: 14, color: Colors.grey),
+            ),
+          ],
+        ),
       ),
     );
   }
