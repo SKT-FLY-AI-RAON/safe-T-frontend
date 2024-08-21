cd3710aed1df9af297aeccc3d964f45542ffc9ae
juwowa09 <juwowa09@gmail.com>
2024-08-21 09:03:53 +0900

    ju: 튜토리얼 페이지 수정

--- lib/selectWarnings.dart
+++ lib/selectWarnings.dart
@@ -33,93 +33,97 @@ class _SelectWarningState extends State<SelectWarning> {
         title: Text('경고 방식 선택'),
         centerTitle: true,
       ),
-      body: ListView(
+      body: Column(
         children: <Widget>[
-          Container(
-            decoration: BoxDecoration(
-              border: Border(
-                bottom: BorderSide(color: Colors.grey.shade300, width: 1.5), // 하단 회색 선 추가
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(left: 20.0, top: 10.0, bottom: 10.0),
-              child: RadioListTile<int>(
-                title: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '페달 블랙박스 화면 보여주기',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(20.0),
-                      child: Image.asset(
-                        'assets/warning-1.png',
-                        width: 200,
-                        height: 300,
-                        fit: BoxFit.fill,
-                      ),
-                    ),
-                  ],
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(color: Colors.grey.shade300, width: 1.5), // 하단 회색 선 추가
                 ),
-                value: 1,
-                groupValue: _selectedWarningMethod,  // _selectedWarningMethod는 int 타입이어야 합니다.
-                onChanged: (int? value) {
-                  setState(() {
-                    _selectedWarningMethod = value;  // value도 int 타입이어야 합니다.
-                    print(_selectedWarningMethod);
-                  });
-                },
-                controlAffinity: ListTileControlAffinity.trailing,
-                activeColor: Colors.blueAccent,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20.0, top: 10.0, bottom: 10.0),
+                child: RadioListTile<int>(
+                  title: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '페달 블랙박스 화면 보여주기',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(20.0),
+                        child: Image.asset(
+                          'assets/warning-1.png',
+                          width: 200,
+                          height: 300,
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    ],
+                  ),
+                  value: 1,
+                  groupValue: _selectedWarningMethod,  // _selectedWarningMethod는 int 타입이어야 합니다.
+                  onChanged: (int? value) {
+                    setState(() {
+                      _selectedWarningMethod = value;  // value도 int 타입이어야 합니다.
+                      print(_selectedWarningMethod);
+                    });
+                  },
+                  controlAffinity: ListTileControlAffinity.trailing,
+                  activeColor: Colors.blueAccent,
+                ),
               ),
             ),
           ),
-          Container(
-            decoration: BoxDecoration(
-              border: Border(
-                bottom: BorderSide(color: Colors.grey.shade300, width: 1.5),
-              ),
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(left: 20.0, top: 10.0, bottom: 10.0),
-              child: RadioListTile<int>(
-                title: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      '내비 화면에서 경고창 보이기',
-                      style: TextStyle(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w400,
-                      ),
-                    ),
-                    SizedBox(height: 10),
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(20.0),
-                      child: Image.asset(
-                        'assets/warning-2.png',
-                        width: 200,
-                        height: 300,
-                        fit: BoxFit.fill,
-                      ),
-                    ),
-                  ],
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                border: Border(
+                  bottom: BorderSide(color: Colors.grey.shade300, width: 1.5),
                 ),
-                value: 2,
-                groupValue: _selectedWarningMethod,
-                onChanged: (int? value) {
-                  setState(() {
-                    _selectedWarningMethod = value;
-                    print(_selectedWarningMethod);
-                  });
-                },
-                controlAffinity: ListTileControlAffinity.trailing,
-                activeColor: Colors.blueAccent,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(left: 20.0, top: 10.0, bottom: 10.0),
+                child: RadioListTile<int>(
+                  title: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        '내비 화면에서 경고창 보이기',
+                        style: TextStyle(
+                          fontSize: 18,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(20.0),
+                        child: Image.asset(
+                          'assets/warning-2.png',
+                          width: 200,
+                          height: 300,
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                    ],
+                  ),
+                  value: 2,
+                  groupValue: _selectedWarningMethod,
+                  onChanged: (int? value) {
+                    setState(() {
+                      _selectedWarningMethod = value;
+                      print(_selectedWarningMethod);
+                    });
+                  },
+                  controlAffinity: ListTileControlAffinity.trailing,
+                  activeColor: Colors.blueAccent,
+                ),
               ),
             ),
           ),
