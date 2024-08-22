3d7ff29b99d7d750a21b37324518e8f93114c98e
YuJin <tokyj515@gmail.com>
2024-08-22 14:52:53 +0900

    fix: 목적지 선택 페이지 상단바 하얀색으로 변경

--- lib/firstscreen.dart
+++ lib/firstscreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart'; // 시스템 UI 스타일 변경을 위해 필요
 import 'boramae.dart';
 import 'map_screen.dart';
 
@@ -7,23 +8,29 @@ class firstscreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    // 상태바 배경을 하얗게, 아이콘을 검정색으로 설정
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Colors.white, // 상태바 배경색을 흰색으로 설정
+      statusBarIconBrightness: Brightness.dark, // 상태바 아이콘 색상을 검정색으로 설정
+      statusBarBrightness: Brightness.light, // iOS용 (밝은 상태바는 검정 아이콘을 의미)
+    ));
+
     return SafeArea(
       child: Scaffold(
-        body:Stack(
+        body: Stack(
           children: [
             Image.asset(
+              'assets/firstscreen.png',
               height: MediaQuery.of(context).size.height,
-              width:MediaQuery.of(context).size.height,
-              'assets/firstscreen.png',fit:BoxFit.fill,
+              width: MediaQuery.of(context).size.width, // 너비를 화면 너비로 설정
+              fit: BoxFit.fill,
             ),
             DraggableScrollableSheet(
-              // expand: false, // 화면 공간 차지와 비율
               initialChildSize: 0.4, // 초기 크기 (화면 높이의 40%)
               minChildSize: 0.1, // 최소 크기 (화면 높이의 10%)
               maxChildSize: 0.8, // 최대 크기 (화면 높이의 80%)
               builder: (BuildContext context, ScrollController scrollController) {
                 return Material(
-                  // 이 Material 위젯을 추가
                   elevation: 4,
                   borderRadius: BorderRadius.vertical(
                     top: Radius.circular(20),
@@ -36,7 +43,7 @@ class firstscreen extends StatelessWidget {
                       child: Column(
                         crossAxisAlignment: CrossAxisAlignment.start,
                         children: [
-                          // 작대기 추가
+                          // 드래그바 추가
                           Container(
                             height: 30,
                             alignment: Alignment.center,
@@ -93,10 +100,10 @@ class firstscreen extends StatelessWidget {
                                         title: Text('SKT 타워'),
                                         trailing: Icon(Icons.location_pin),
                                         onTap: () {
-                                            Navigator.pushReplacement(
-                                              context,
-                                              MaterialPageRoute(builder: (context) => boramae()),
-                                            );
+                                          Navigator.pushReplacement(
+                                            context,
+                                            MaterialPageRoute(builder: (context) => boramae()),
+                                          );
                                         },
                                       ),
                                     ],
@@ -118,4 +125,3 @@ class firstscreen extends StatelessWidget {
     );
   }
 }
-
